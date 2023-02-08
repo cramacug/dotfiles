e0b840de3d03f14deb1241e0a72e91c137ab2d2b
Marc CG <mcabreguinart@gmail.com>
2023-02-08 15:31:55 +0000

    [zshrc] - Add microk8s shortcuts

---
+++ zsh/zsh_configs/microk8s.zsh
@@ -0,0 +1,14 @@
+
+# k8s
+alias k=kubectl
+
+## Microk8s shortcuts
+# alias k='microk8s kubectl'
+# alias kg='microk8s kubectl get'
+# alias ka='microk8s kubectl apply -f'
+# alias kd='microk8s kubectl describe'
+# alias kdel='microk8s kubectl delete -f'
+# alias klogs='microk8s kubectl logs'
+# source ~/.bash_aliases
+alias helm='microk8s helm'
+
