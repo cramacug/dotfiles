96b25c8e642f155d9a1b3054a0bcefab7f8acd96
Marc CG <marc.cabre@dexma.com>
2023-06-30 13:42:39 +0200

    [docker] - Add shorcuts for docker-compose

--- zsh/zsh_configs/common/global.zsh
+++ zsh/zsh_configs/common/global.zsh
@@ -1,1 +1,5 @@
 alias test_config_linux='echo global config configuration successfully created'
+
+alias dcud='docker-compose --file ./docker-compose.yml up --detach'
+alias dcd='docker-compose --file ./docker-compose.yml down'
+alias dcda='docker-compose --file ./docker-compose.yml down --rmi all --volumes --remove-orphans'
