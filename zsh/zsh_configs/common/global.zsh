b953011e90399192537de4b31b1a1ede6535d0fb
Marc CG <marc.cabre@dexma.com>
2023-07-06 10:05:42 +0200

    [zshrc] - Add docker compose and update time aliases

--- zsh/zsh_configs/common/global.zsh
+++ zsh/zsh_configs/common/global.zsh
@@ -1,5 +1,5 @@
 alias test_config_linux='echo global config configuration successfully created'
 
-alias dcud='docker-compose --file ./docker-compose.yml up --detach'
-alias dcd='docker-compose --file ./docker-compose.yml down'
-alias dcda='docker-compose --file ./docker-compose.yml down --rmi all --volumes --remove-orphans'
+alias dcud='docker compose -f ./docker-compose.yml up --detach'
+alias dcd='docker compose -f ./docker-compose.yml down'
+alias dcda='docker compose -f ./docker-compose.yml down --rmi all --volumes --remove-orphans'
