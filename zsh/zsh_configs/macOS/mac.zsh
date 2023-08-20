048fc00908840c48951716922a3a0d1e61917878
Marc Cabré Guinart <marc_cg@Marcs-MacBook-Pro.local>
2023-08-20 19:22:42 +0200

    [zshrc] Add Docker Labs Debug Tools CLI

--- zsh/zsh_configs/macOS/mac.zsh
+++ zsh/zsh_configs/macOS/mac.zsh
@@ -1,3 +1,7 @@
 alias test_config_macOS='echo macOS configuration successfully created'
 
 alias ll='ls -lhH'
+
+# Added by Docker Labs Debug Tools"
+export PATH="$PATH:/Users/marc_cg/.local/bin" 
+
