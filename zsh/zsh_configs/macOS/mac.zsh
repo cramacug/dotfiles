45941342b759339037bc2745af2c6fbb06d181a4
Marc CG <marc.cabre@dexma.com>
2023-10-04 13:55:07 +0200

    [zshrc] - Use HOME env instead of canonical path

--- zsh/zsh_configs/macOS/mac.zsh
+++ zsh/zsh_configs/macOS/mac.zsh
@@ -3,5 +3,5 @@ alias test_config_macOS='echo macOS configuration successfully created'
 alias ll='ls -lhH'
 
 # Added by Docker Labs Debug Tools"
-export PATH="$PATH:/Users/marc_cg/.local/bin" 
+export PATH="$PATH:$HOME/.local/bin" 
 
