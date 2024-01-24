86edd140f7526e4d97e82a51a8e38b5ae4c7cab3
marc_cg <mcabreguinart@gmail.com>
2024-01-24 13:46:36 +0100

    [macOs] - Set node with different paths for Apple Silicon amb Intel

--- zsh/zsh_configs/macOS/mac.zsh
+++ zsh/zsh_configs/macOS/mac.zsh
@@ -5,5 +5,8 @@ alias ll='ls -lhH'
 # Added by Docker Labs Debug Tools"
 export PATH="$PATH:$HOME/.local/bin" 
 
+# Apple Silicon 
 export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
+# Apple Intel
+export PATH="/usr/local/opt/node@20/bin:$PATH"
 
