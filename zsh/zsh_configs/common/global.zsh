72cabd68df392c9c9bbc528169eed7cb313f1597
Marc CG <marc.cabre@dexma.com>
2024-09-04 15:12:30 +0200

    [docker] - drmall & [ideavimc] - Test shorcuts changes

--- zsh/zsh_configs/common/global.zsh
+++ zsh/zsh_configs/common/global.zsh
@@ -6,4 +6,7 @@ alias dcdri='docker compose --file ./docker-compose.yml down --rmi all'
 alias dcdrv='docker compose --file ./docker-compose.yml down --volumes --remove-orphans'
 alias dcdra='docker compose --file ./docker-compose.yml down --rmi all --volumes --remove-orphans'
 # alias docker_psf='docker ps --format "{{.ID}} -- {{.State}} -- {{.Ports}} -- {{.Names}}"'
-alias dcdidea='find -E ~/IdeaProjects -type file -regex ".*docker-compose.ya?ml" -print0 | xargs -0 -I X docker compose --file X down'
+# alias dcdidea='find -E ~/IdeaProjects -type file -regex ".*docker-compose.ya?ml" -print0 | xargs -0 -I X docker compose --file X down'
+# alias dcdidea='find -E ~/IdeaProjects -type file -regex ".*docker-compose.ya?ml" -print0 | xargs -t -0 -I X docker compose --file X down | echo "------\n" '
+
+alias drall='docker ps --quiet --all | xargs -I X docker stop X | xargs -I X docker rm X'
